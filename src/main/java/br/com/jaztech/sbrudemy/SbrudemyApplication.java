93c8b65f64459efd93864f615f60cecf633e990e
Tiago Jasper Zuege <tiago.zuege@gmail.com>
2020-02-16 11:26:20 -0300

    Commit inicial

---
+++ src/main/java/br/com/jaztech/sbrudemy/SbrudemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.jaztech.sbrudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbrudemyApplication.class, args);
+	}
+
+}
