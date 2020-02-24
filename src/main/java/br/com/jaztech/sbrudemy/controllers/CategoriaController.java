279aa2862546f8ec305ae811bf5957844192b2f9
Tiago Jasper Zuege <tiago.zuege@gmail.com>
2020-02-23 21:37:09 -0300

    Teste classe de domínio 'Categoria'

--- src/main/java/br/com/jaztech/sbrudemy/controllers/CategoriaController.java
+++ src/main/java/br/com/jaztech/sbrudemy/controllers/CategoriaController.java
@@ -1,15 +1,26 @@
 package br.com.jaztech.sbrudemy.controllers;
 
+import br.com.jaztech.sbrudemy.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
